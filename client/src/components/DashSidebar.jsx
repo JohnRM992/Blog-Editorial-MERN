89b9dbab67e756673475efae897c0a15beaa7dcc
JohnRM992 <jonatanrm92@gmail.com>
2024-06-24 05:52:28 -0300

    Add Post section to Dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -2,10 +2,14 @@ import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 // import { Sidebar } from 'flowbite-react';
 import { FaUserCircle } from "react-icons/fa";
-import { IoMdLogOut } from "react-icons/io";
+// import { IoMdLogOut } from "react-icons/io";
+import { MdEditDocument } from "react-icons/md";
+import { useSelector , useDispatch } from 'react-redux'
 
 export default function DashSidebar() {
   const location = useLocation();
+  const dispatch = useDispatch();
+  const { currentUser} = useSelector(state => state.user)
   const [tab, setTab] = useState("");
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -18,16 +22,29 @@ export default function DashSidebar() {
   return (
     <div className="w-full md:w-60  ">
       <Link to="/dashboard?tab=profile" as='div'>
-        <button className="rounded-md flex pl-5 pt-3 text-[1D1D03] bg-[#F7F5E8] w-11/12 h-14 mt-5 ml-4 md:ml-2.5   focus:border-black focus:border-2">
+        <button 
+        
+        className="rounded-md flex pl-5 pt-3 text-[1D1D03] bg-[#F7F5E8] w-11/12 h-14 mt-5 ml-4 md:ml-2.5   focus:border-black focus:border-2">
           <FaUserCircle className="w-8 h-8" />
-          <p className="font-medium pt-1">Perfil</p>
+          <p className="font-medium pt-1 ml-2">Perfil</p>
         </button>
       </Link>
 
-      <button className="rounded-md flex pl-5 pt-3 text-[1D1D03] bg-[#F7F5E8] w-11/12 h-14 mt-5 ml-4 md:ml-2.5  mb-5 focus:border-black focus:border-2">
+      {currentUser.isAdmin && (
+           <Link to='/dashboard?tab=posts' as='div'>
+           <button className="rounded-md flex pl-5 pt-3 text-[1D1D03] bg-[#F7F5E8] w-11/12 h-14 mt-5 ml-4 md:ml-2.5   focus:border-black focus:border-2 mb-3">
+               <MdEditDocument className="w-8 h-8" />
+               <p className="font-medium pt-1 ml-2">Posts</p>
+             </button>
+           </Link>
+      )}
+     
+
+
+      {/* <button className="rounded-md flex pl-5 pt-3 text-[1D1D03] bg-[#F7F5E8] w-11/12 h-14 mt-5 ml-4 md:ml-2.5  mb-5 focus:border-black focus:border-2">
         <IoMdLogOut className="w-8 h-8" />
         <p className="font-medium pt-1">Logout</p>
-      </button>
+      </button> */}
     </div>
   );
 }
