25eb587f1ab004adfb7d0904b24546132d8c790a
JohnRM992 <jonatanrm92@gmail.com>
2024-06-25 17:58:48 -0300

    Aded Get Users api route

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 // Importación de controllers
-import { test , updateUser , deleteUser ,signout} from '../controllers/user.controller.js';
+import { test , updateUser , deleteUser ,signout ,getUsers} from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -11,6 +11,6 @@ router.get('/test', test  )
 router.put('/update/:userId' ,verifyToken, updateUser)
 router.delete('/delete/:userId' , verifyToken , deleteUser)
 router.post('/signout' , signout)
-
+router.get('/getusers' , verifyToken , getUsers)
 
 export default router;
